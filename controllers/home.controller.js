b06e75f44910035ef2769efee34f4fb30aa52c3b
DorukEmre <doruk.emre@outlook.com>
2022-09-27 16:32:51 +0100

    Added different ways to export module

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -3,3 +3,18 @@ module.exports = {
         res.render('index.ejs')
     }
 }
+
+// --------------
+
+// exports.getIndex = (req,res)=>{
+//       res.render('index.ejs')
+//   }
+
+// --------------
+
+// const getIndex = (req,res)=>{
+//       res.render('index.ejs')
+//   }
+// module.exports = getIndex
+
+// --------------
